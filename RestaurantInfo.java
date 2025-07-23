3d76fe7c1e6a0974834a44230d677f93885c49e8
mourningglorii <137913336+mourningglorii@users.noreply.github.com>
2025-07-22 20:46:05 -0400

    Add files via upload

---
+++ RestaurantInfo.java
@@ -0,0 +1,7 @@
+public class RestaurantInfo {
+    public static void main(String[] args) {
+        System.out.println("Restaurant: Seaside Grill");
+        System.out.println("Address: 123 Oceanview Drive");
+        System.out.println("City, State, Zip: Wilmington, NC 28401");
+    }
+}
